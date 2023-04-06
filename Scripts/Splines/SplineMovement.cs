a85dfdff61f6e1001ab0bdfe89c30f697a9da9cf
YefersonCC <ycc@gmail.com>
2023-04-06 08:57:58 -0500

    Spline mesh improved

---
+++ Scripts/Splines/SplineMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Splines
+{
+   public class SplineMovement : MonoBehaviour
+   {
+      [SerializeField]
+      private float speed;
+
+      private float distanceTraveled;
+
+      [SerializeField]
+      private SplineMesh refSplineMesh;
+
+      public float Speed
+      {
+         get => speed;
+         set => speed = value;
+      }
+
+      public float DistanceTraveled
+      {
+         get => distanceTraveled;
+         set => distanceTraveled = value;
+      }
+
+      private void Update()
+      {
+         distanceTraveled += speed * Time.deltaTime;
+         transform.position = refSplineMesh.GetPositionAtDistance(distanceTraveled);
+         transform.rotation = refSplineMesh.GetOrientationAtDistance(distanceTraveled);
+      }
+   }
+}
