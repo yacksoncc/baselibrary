47060c49f5c9f15d75013246af2b6a3e73d3ca0c
YefersonCC <ycc@gmail.com>
2023-05-26 14:34:57 -0500

    ScriptableEventUshor added

---
+++ Assets/Scripts/ScriptableEvents/ScriptableEventUshort.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ScriptableEvents
+{
+   [CreateAssetMenu(fileName = "seUshort", menuName = "ScriptableEvents/ScriptableEventUshort", order = 0)]
+   public class ScriptableEventUshort : ScriptableEvent<ushort>
+   {
+        
+   }
+}
