5d56bc2cbeeaa6df24cc0e04a677013b59d0f6c1
YefersonCC <ycc@gmail.com>
2023-05-26 09:08:26 -0500

    Scriptable event byte added

---
+++ Scripts/ScriptableEvents/ScriptableEventByte.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace ScriptableEvents
+{
+   [CreateAssetMenu(fileName = "seByte", menuName = "ScriptableEvents/ScriptableEventByte", order = 0)]
+   public class ScriptableEventByte : ScriptableEvent<byte>
+   {
+      
+   }
+}
