87d6d9bfa870706c0fcf197662d193f0d4521f35
YefersonCC <ycc@gmail.com>
2023-07-25 19:53:16 -0500

    Add random position Circunference

--- Assets/Scripts/Extensions/ExtensionRamdom.cs
+++ Assets/Scripts/Extensions/ExtensionRamdom.cs
@@ -12,12 +12,23 @@ namespace Extensions
       public static Vector3 RandomPointXZInSphere(float argScale = 1)
       {
          var tmpRandomAngle = Random.Range(0, Mathf.PI * 2);
-         return new Vector3(Mathf.Cos(tmpRandomAngle), 0, Mathf.Sin(tmpRandomAngle)) * argScale;
+         return new Vector3(Mathf.Cos(tmpRandomAngle), 0, Mathf.Sin(tmpRandomAngle)) * Random.Range(0f, argScale);
       }
 
       public static Vector3 RandomPointXZInSphereAroundPosition(Vector3 argPosition, float argScale = 1)
       {
          return RandomPointXZInSphere(argScale) + argPosition;
+      }
+
+      public static Vector3 RandomPointXZInCircunference(float argScale = 1)
+      {
+         var tmpRandomAngle = Random.Range(0, Mathf.PI * 2);
+         return new Vector3(Mathf.Cos(tmpRandomAngle), 0, Mathf.Sin(tmpRandomAngle)) * argScale;
+      }
+
+      public static Vector3 RandomPointXZInCircunferenceAroundPosition(Vector3 argPosition, float argScale = 1)
+      {
+         return RandomPointXZInCircunference(argScale) + argPosition;
       }
    }
 }
