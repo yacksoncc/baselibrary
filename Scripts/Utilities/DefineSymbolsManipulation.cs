0207306a0fa3d78e545e3275dde9a91850a3bd73
YefersonCC <ycc@gmail.com>
2023-12-18 17:17:46 -0500

    Add Define Symbols Manipulation

---
+++ Scripts/Utilities/DefineSymbolsManipulation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace Utilities
+{
+   public class DefineSymbolsManipulation
+   {
+      public static bool RemoveOrAddDefine(string argDefineSymbol, bool argRemoveDefineSymbol)
+      {
+         var tmpCurrentDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+         var tmpHashSetDefines = new HashSet<string>();
+         var tmpCurrentArr = tmpCurrentDefines.Split(';');
+
+         //Add any define which doesn't contain MIRROR.
+         foreach(string item in tmpCurrentArr)
+            tmpHashSetDefines.Add(item);
+
+         var tmpStartingCount = tmpHashSetDefines.Count;
+
+         if(argRemoveDefineSymbol)
+            tmpHashSetDefines.Remove(argDefineSymbol);
+         else
+            tmpHashSetDefines.Add(argDefineSymbol);
+
+         bool modified = (tmpHashSetDefines.Count != tmpStartingCount);
+
+         if(modified)
+         {
+            string changedDefines = string.Join(";", tmpHashSetDefines);
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, changedDefines);
+         }
+
+         return modified;
+      }
+   }
+}
