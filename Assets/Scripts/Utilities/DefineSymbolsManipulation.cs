cb61b7bdd1dd13959d2c7611ea495ff6ea5a9042
YefersonCC <ycc@gmail.com>
2023-12-18 21:23:37 -0500

    Define Symbols manipulation to editor mode only

--- Assets/Scripts/Utilities/DefineSymbolsManipulation.cs
+++ Assets/Scripts/Utilities/DefineSymbolsManipulation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
 
 namespace Utilities
@@ -34,3 +35,4 @@ namespace Utilities
       }
    }
 }
+#endif
