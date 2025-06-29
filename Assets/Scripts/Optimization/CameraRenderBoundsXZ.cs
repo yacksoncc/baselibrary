4f5d83d82b396dc58dd465180d3a218cc509b668
YefersonCC <ycc760@gmail.com>
2025-06-28 20:25:38 -0500

    Implement render around camera

--- Assets/Scripts/Optimization/CameraRenderBoundsXZ.cs
+++ Assets/Scripts/Optimization/CameraRenderBoundsXZ.cs
@@ -9,90 +9,107 @@ namespace Optimization
       [Header("Setup")]
       [SerializeField]
       private Camera refCameraToRender;
-
+   
       [SerializeField]
       private float thresholdToUpdateValues = 0.4f;
-
+   
       [SerializeField]
       private Transform transformPlaneFloor;
-
-      private Plane planeGround;
-
-      private Bounds boundsRender;
-
-      private Vector2 previousPositionCameraXZ;
-
+   
+      [Header("Raycast-based mode")]
       [SerializeField]
-      private Vector3 expandBoundsAmount = new Vector3(1, 0, 1);
+      private Vector3 expandBoundsAmount = new (1, 0, 1);
+   
+      [Header("Radius-based mode")]
+      [Tooltip("Radio (m) que se encapsula alrededor de la cámara cuando RenderModeBoundsXZ es true")]
+      [SerializeField]
+      private float radiusAroundCamera = 40;
+      
+      private Plane planeGround;
+      
+      private Bounds boundsRender;
+      
+      private Vector2 previousPositionCameraXZ;
+      
+      private bool boundsWasUpdatedTheLastFrame;
+      
+      public Bounds BoundsRender => boundsRender;
 
       [Header("Events")]
       [SerializeField]
       private ScriptableEventEmpty seBoundsRenderUpdated;
-
-      private bool boundsWasUpdatedTheLastFrame;
-
-      public Bounds BoundsRender
-         => boundsRender;
-
+      
+      public bool RenderModeAroundBoundsXZ { get; set; } = false;
+   
       private void OnEnable()
       {
          var tmpPositionPlaneFloor = transformPlaneFloor.position;
          boundsRender = new Bounds(tmpPositionPlaneFloor, Vector3.one);
          planeGround = new Plane(transformPlaneFloor.up, tmpPositionPlaneFloor);
       }
-
+   
       private void OnDrawGizmos()
       {
          Gizmos.DrawCube(boundsRender.center, boundsRender.size);
       }
-
+   
       private void Update()
       {
          UpdateBoundsCenter();
       }
-
+   
       private void UpdateBoundsCenter()
       {
          var tmpCameraPositionXYZ = refCameraToRender.transform.position;
-         var tmpPositionXZ = new Vector2(tmpCameraPositionXYZ[0], tmpCameraPositionXYZ[2]);
-
-         if((tmpPositionXZ - previousPositionCameraXZ).magnitude > thresholdToUpdateValues)
+         var tmpPositionXZ = new Vector2(tmpCameraPositionXYZ.x, tmpCameraPositionXYZ.z);
+   
+         if((tmpPositionXZ - previousPositionCameraXZ).magnitude <= thresholdToUpdateValues)
+            return;
+   
+         previousPositionCameraXZ = tmpPositionXZ;
+         boundsWasUpdatedTheLastFrame = true;
+   
+         if(RenderModeAroundBoundsXZ) 
          {
-            previousPositionCameraXZ = tmpPositionXZ;
-            boundsWasUpdatedTheLastFrame = true;
-
-            var tmpRayFromCenterScreen = refCameraToRender.ScreenPointToRay(new Vector2(Screen.width * 0.5f, Screen.width * 0.5f));
+            boundsRender.center = new Vector3(tmpCameraPositionXYZ.x, 0f, tmpCameraPositionXYZ.z);
+            var tmpDiameter = radiusAroundCamera * 2f;
+            boundsRender.size = new Vector3(tmpDiameter, 1f, tmpDiameter);
+         }
+         else
+         {
+            var tmpRayFromCenterScreen = refCameraToRender.ScreenPointToRay(
+                                            new Vector2(Screen.width * 0.5f, Screen.width * 0.5f));
             planeGround.Raycast(tmpRayFromCenterScreen, out var tmpDistanceToPlane);
             var tmpPositionCenter = tmpRayFromCenterScreen.GetPoint(tmpDistanceToPlane);
-            boundsRender.center = new Vector3(tmpPositionCenter[0], 0, tmpPositionCenter[2]);
+            boundsRender.center = new Vector3(tmpPositionCenter.x, 0f, tmpPositionCenter.z);
             boundsRender.size = Vector3.one;
-
+   
             var tmpBounds0_0Ray = refCameraToRender.ScreenPointToRay(new Vector3(0, 0));
             planeGround.Raycast(tmpBounds0_0Ray, out var tmpDistanceEnter);
             boundsRender.Encapsulate(tmpBounds0_0Ray.GetPoint(tmpDistanceEnter));
-
+   
             var tmpBounds1_0Ray = refCameraToRender.ScreenPointToRay(new Vector3(Screen.width, 0));
             planeGround.Raycast(tmpBounds1_0Ray, out tmpDistanceEnter);
             boundsRender.Encapsulate(tmpBounds1_0Ray.GetPoint(tmpDistanceEnter));
-
+   
             var tmpBounds1_1Ray = refCameraToRender.ScreenPointToRay(new Vector3(Screen.width, Screen.height));
             planeGround.Raycast(tmpBounds1_1Ray, out tmpDistanceEnter);
             boundsRender.Encapsulate(tmpBounds1_1Ray.GetPoint(tmpDistanceEnter));
-
+   
             var tmpBounds0_1Ray = refCameraToRender.ScreenPointToRay(new Vector3(0, Screen.height));
             planeGround.Raycast(tmpBounds0_1Ray, out tmpDistanceEnter);
             boundsRender.Encapsulate(tmpBounds0_1Ray.GetPoint(tmpDistanceEnter));
-
+   
             boundsRender.Expand(expandBoundsAmount);
          }
-
+   
          if(boundsWasUpdatedTheLastFrame)
          {
             boundsWasUpdatedTheLastFrame = false;
             seBoundsRenderUpdated.ExecuteEvent();
          }
       }
-
+   
       public bool GetIfWorldPositionCanBeRendered(Vector3 argWorldPosition)
       {
          return boundsRender.Contains(argWorldPosition);
